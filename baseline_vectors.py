02c5d54e4a539bed668122ede95130beebab7c69
Joseph Smith <joseph.smith@tessella.com>
2021-07-06 13:53:22 +0100

    Telescope model class, stores cart coord and dim

---
+++ baseline_vectors.py
@@ -0,0 +1,68 @@
+## BASELINE VECTOR DETERMINATION ##
+
+import numpy as np
+import sys, os
+
+class TelescopeModel():
+
+    def __init__(self):
+
+        self.txt_path = "antennae_coordinates.txt"
+        self.input_dimensions = None
+        self.coord_matrix = None
+
+        self.extractInputData()
+        
+    def extractInputData(self):
+        """
+        Extracts coordinate values and length dimension of antennae distribution from text file
+
+        Parameters
+        -------
+        self.txt_path : str 
+            path to text file containing input data
+        
+        Returns
+        -------
+        self.input_dimensions : int
+            length dimension of coordinates - m (metres) or ft (feet)
+        self.coord_matrix : np.array
+            2D numpy matrix containing cartesian coordinates of antennae
+
+        """
+        # Open text file from same directory
+        try:
+            text = open(os.path.join(sys.path[0], self.txt_path), "r")
+        except:
+            print("Text file could not be opened")
+            text = []
+
+        coord_list = []
+        count = 0
+        for i, line in enumerate(text):
+            # Extract length dimension
+            if line.startswith(">") and count == 0:
+                count += 1
+                dim = next(text).replace("\n", "")
+                if dim == "m" or dim == "ft":
+                    self.input_dimensions = next(text).replace("\n", "")
+                else:
+                    print("Invalid length dimension")
+                    break
+            # Extract coordinates  
+            elif line.startswith(">") == False:
+                try:
+                    line = line.replace("\n", "")
+                    x = line.split(",")[0].replace("(", "")
+                    y = line.split(",")[1].replace(")", "")
+                    coord = (int(x), int(y))
+                except:
+                    print("Invalid coordinates")
+                    coord_list = []
+                    break
+                coord_list.append(coord)
+
+        self.coord_matrix = np.array(coord_list)
+        print(self.coord_matrix)
+
+TelescopeModel()
